89aca0ae9c6be722e932e7101d96f378fb2f16b4
Stephen Beckstrand <spbeckstrand@gmail.com>
2022-12-01 07:46:13 -0700

    Added day01 solution

---
+++ day01/day01.py
@@ -0,0 +1,28 @@
+f = open("input.txt", "r")
+calorie_list = f.readlines()
+
+  
+def most_calories(calorie_list, carrier_count):
+    max_cal = 0
+    cur_cal = 0
+    elf_cals = []
+
+    for line in calorie_list:
+        if line.strip():
+            try:
+                cur_cal += int(line)
+            except:
+                print("Invalid input")
+        else:
+            if cur_cal > max_cal:
+                max_cal = cur_cal
+            
+            elf_cals.append(cur_cal)
+            cur_cal = 0
+    
+    elf_cals.sort(reverse=True)
+    return sum(elf_cals[0:carrier_count])
+
+
+print(most_calories(calorie_list, 1))
+print(most_calories(calorie_list, 3))
