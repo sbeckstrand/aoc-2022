7bd92994726ea735f4b452e9e40202f3952cd893
Stephen Beckstrand <spbeckstrand@gmail.com>
2022-12-03 13:32:32 -0700

    Updated day03 to include solution for part 2

--- day03/day03.py
+++ day03/day03.py
@@ -1,19 +1,33 @@
 input = open("input.txt", "r")
-rucksacks = input.readlines()
+rucksacks = input.read().splitlines()
 input.close()
 
-def calc_duplicate_priority(rucksacks):
+def calc_priority(rucksacks, condition=1):
     priority_count = 0
-    for rucksack in rucksacks:
-        rucksack = rucksack.strip()
-        comp1 = rucksack[:len(rucksack)//2]
-        comp2 = rucksack[len(rucksack)//2:]
-        common = list(set(comp1).intersection(comp2))
+    interval = 1
+    if condition == 2:
+        interval = 3
+
+    # If condition one, iterate through each rucksack
+    # If condition two, iterate through each third rucksack
+    for i in range(0, len(rucksacks), interval):
+        groups = []
+        # If condition one, split each rucksack in half. 
+        # If condition two, split rucksacks groups of three.
+        if condition == 1:
+            rucksack = rucksacks[i]
+            groups.append(rucksack[:len(rucksack)//2]) 
+            groups.append(rucksack[len(rucksack)//2:])
+        elif condition == 2:
+            groups = rucksacks[i:i + 3]
+        
+        # Find common value by converting to sets and finding intersection between each group
+        common = list(set(groups[0]).intersection(*groups[0:]))
+        
+        # Find priority of common character by ascii code
         priority_count += ord(common[0]) - (96 if common[0].islower() else 38)
 
-
-
     return priority_count
-        
     
-print(calc_duplicate_priority(rucksacks))
+print(calc_priority(rucksacks))
+print(calc_priority(rucksacks, 2))
