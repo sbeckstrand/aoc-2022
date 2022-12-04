4473af059f594dfa3ba9e11b7316118a22790134
Stephen Beckstrand <spbeckstrand@gmail.com>
2022-12-03 23:47:29 -0700

    adjusted solution for day04

--- day04/day04.py
+++ day04/day04.py
@@ -10,11 +10,12 @@ def find_overlapping_assignments(assignment_pairs, overlap_type="full"):
             list_assignment = assignment.split("-")
             assignments.append(set(range(int(list_assignment[0]), int(list_assignment[1]) + 1)))
         
+        assn_overlap_durration = len(assignments[0].intersection(assignments[1]))
         if overlap_type == "full":
-            if len(assignments[0].intersection(assignments[1])) == len(assignments[0]) or len(assignments[0].intersection(assignments[1])) == len(assignments[1]):
+            if assn_overlap_durration in [len(assignments[0]), len(assignments[1])]:
                 contained_count += 1
         elif overlap_type == "any":
-            if len(assignments[0].intersection(assignments[1])) > 0:
+            if assn_overlap_durration > 0:
                 contained_count += 1
     
     return contained_count
